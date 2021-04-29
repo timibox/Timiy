b16cee2e41a8bcc99c52d2f39bd234d61e0b8aeb
timibox <e-mmal@live.com>
2021-04-29 12:41:41 +0800

    Signed-off-by: timibox <e-mmal@live.com>

--- themes/hexo-timiy/source/js/console.js
+++ themes/hexo-timiy/source/js/console.js
@@ -1,3 +1,3 @@
 
-console.log("Timiy | QQ:4370142  https://timiy.cn\n分享 互助 共赢 - 为创造而生\n\n机器人定制开发联系 https://wpa.qq.com/msgrd?v=3&uin=4370142&site=qq&menu=yes")
-console.log("%c ", "background: url(https://cdn.jsdelivr.net/gh/htmlmi/cdn@latest/2021/04/29/461de8f76409f423f430db5096891df2.png) no-repeat center;padding-left:640px;padding-bottom: 242px;")
+console.log("\nTimiy | QQ:4370142  https://timiy.cn\n分享 互助 共赢 - 为创造而生\n\n机器人定制开发联系 https://wpa.qq.com/msgrd?v=3&uin=4370142&site=qq&menu=yes")
+console.log("%c ", "background: url(https://cdn.jsdelivr.net/gh/htmlmi/cdn@latest/2021/04/29/461de8f76409f423f430db5096891df2.png) no-repeat center;padding-left:870px;padding-bottom: 245px;")
