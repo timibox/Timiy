8ea90cae339a0de5f6e3eee35f381304d4af88c7
timibox <e-mmal@live.com>
2021-04-29 12:05:40 +0800

    Signed-off-by: timibox <e-mmal@live.com>

--- themes/hexo-timiy/source/js/console.js
+++ themes/hexo-timiy/source/js/console.js
@@ -1,2 +1,3 @@
 
-console.log("【Timiy】 https://timiy.cn \n分享 互助 共赢 - 为创造而生")
+console.log("https://timiy.cn \n分享 互助 共赢 - 为创造而生")
+console.log("%c ", "background: url(https://img.bizhiku.net/uploads/2020/1118/1-20111P92024.jpg?x-oss-process=style/w_870-h_870) no-repeat center;padding-left:640px;padding-bottom: 242px;")
