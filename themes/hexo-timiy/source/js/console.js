c434f384420d0c86228de4a80aa5763006b00b1b
timibox <e-mmal@live.com>
2021-04-29 11:27:26 +0800

    Signed-off-by: timibox <e-mmal@live.com>

---
+++ themes/hexo-timiy/source/js/console.js
@@ -0,0 +1,4 @@
+
+console.log("%c 前端开发 %c calamus.xyz %c 蒲", "color:red","","color:orange;font-weight:bold")
+
+console.log("%c 【前端开发】 %c calamus.xyz %c 蒲", "color:red","","color:orange;font-weight:bold")
